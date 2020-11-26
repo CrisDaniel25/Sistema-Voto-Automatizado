27da44397e0b2084c60f6cbd289dfb9b088655ec
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-11-26 17:39:12 -0400

    [UPDATE]

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <link rel="stylesheet" href="../css/bootstrap-theme.min.css"/>
-        <link rel="stylesheet" href="../css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="../Design/design.css"/>
-        <script src="../js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="Design/design.css"/>
+        <script src="js/bootstrap.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       
