708542c9fb9c684ad0863f5e33c1e4b1f63b4984
verlynPY <verlyn2800@gmail.com>
2020-11-27 08:39:39 -0400

    layout

--- AdminView/AdminHome.php
+++ AdminView/AdminHome.php
@@ -2,6 +2,7 @@
 <?php
 
 require_once '../AdminModel/admin.php';
+require_once '../Design/layout.php';
 
 session_start();
 
@@ -16,8 +17,55 @@ $admin = new Administrador();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../Design/design.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+     <style>
+
+        .divbt:hover{
+            background:#252525;
+            cursor:pointer;
+        }
+
+     </style>
+      
 </head>
 <body>
+
+<div>
+
+<div class="card text-white bg-info mb-2" style="max-width: 15rem;">
+
+  <div class="card-body text-light">
+    <h5 class="card-title">Candidadatos</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div class="divbt">
+  <div class="card-footer bg-transparent border-light ">
     
+    Administrar
+ 
+  </div>
+    </div>
+</div>
+
+<div class="card text-white bg-info mb-2" style="max-width: 15rem;">
+
+  <div class="card-body text-light">
+    <h5 class="card-title">Ciudadanos</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div class="divbt">
+  <div class="card-footer bg-transparent border-light ">
+    
+    Administrar
+ 
+  </div>
+    </div>
+</div>
+
+
+</div>
+
 </body>
 </html>
