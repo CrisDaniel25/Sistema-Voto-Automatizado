29855bf1cf6d02fab5836955acf555009ed88237
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-12-04 19:16:00 -0400

    [UPDATE]

--- AdminView/AdmPuestos/AddPuestos.php
+++ AdminView/AdmPuestos/AddPuestos.php
@@ -1,15 +1,11 @@
 <?php
-require_once '../../Model/PuestoElectoral/puesto.php';
 require_once '../../Data/DataBase.php';
+require_once '../../Model/Ciudadano/ciudadano.php';
 
     $db = new DB();
-    $query = "SELECT * FROM puesto_electivo";
-    $result = $db->connect()->query($query);     
-    if( $result->rowCount() > 0) { while($row = $result->fetch()) { $lastid = $row['puestoid']; } }
-    else {
-        $lastid = 0;        
-    }
-
+    
+    $query = "SELECT * FROM ciudadanos";
+    $result = $db->connect()->query($query);
 ?>
 
 <!DOCTYPE html>
@@ -18,51 +14,40 @@ require_once '../../Data/DataBase.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Voto Automatizado</title>
-    <link rel="stylesheet" href="../../css/bootstrap-theme.min.css"/>
-        <link rel="stylesheet" href="../../css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="../../Design/design.css"/>
-        <link rel="stylesheet" href="../../css/Style.css"/>
-        <script src="../../js/bootstrap.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../../css/bootstrap.css"/>
+    <link rel="stylesheet" href="../../css/bootstrap-theme.min.css"/> 
+    <link rel="stylesheet" href="../../css/bootstrap.min.css"/> 
+    <link rel="stylesheet" href="../../Design/design.css"/>
 </head>
 <body>
 <header>
     <nav class="navbar navbar-dark bg-primary">
     <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto" style="list-style-type: none">
          <li class="nav-item active">
-          <a href="../AdminHome.php" class="btn btn-danger"><span>Volver</span></a>
+          <a href="HomePuestos.php" class="btn btn-danger"><span>Volver</span></a>
          </li>
         </ul>
     </nav>
   </header>
-  <div class="main">
-            <div class="row">
-                <div class="col-text-center">
-                    <div class="col-sm-12">
-                    <form method="POST" action="../../AdminController/AdmPuestos.php">
-                        <div class="form-group">
-                            <input type="text" name="nombre" class="form-control" placeholder="Nombre" required/>
-                        <div>
-                        <div class="form-group">
-                            <input type="text" name="descripcion" class="form-control" placeholder="Descripcion" required/>
-                        <div>
-                        <div class="form-group">
-                            <select class="form-control" name="estado" id="exampleFormControlSelect1">
-                          
-                            <option>Estado</option>
-                            <option value="1">Activo</option>
-                            <option value="0">Inactivo</option>
-                            
-                            </select>
-                           
-                        </div>
-                        <input type="submit" value="Agregar" name="envio" class="btn btn-dark"/>
-                    </form>
-                    </div>
-                </div>
+  
+  <div class="container">
+    <div class="row">
+        <form action="../../AdminController/AdmPuestos.php" method="post" class="form-inline">
+            <div class="form-group row">                         
+                <input class="form-control" type="text" placeholder="Nombre" name="nombre" />
+                <input class="form-control" type="text" placeholder="Descripcion" name="descripcion" />  
+                <select class="form-control" name="estado">   
+                    <option value="" style="display: none;">Estado</option>        
+                    <option value=1>Activo</option>        
+                    <option value=0>Inactivo</option>        
+                </select>           
+
+                <button type="submit" name="envio" class="btn btn-success">Guardar Formulario</button>
+                <button type="reset" value="Clear" class="btn btn-danger">Borrar Formulario</button>
             </div>
-        </div>
+        </form>
+    </div>
+  </div>
 </body>
 </html>
