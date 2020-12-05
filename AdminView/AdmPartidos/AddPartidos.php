7e89cebd81a7268f4bbe748b83d7d6fd91aa021f
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2020-12-05 01:09:14 -0400

    [UPDATE]

--- AdminView/AdmPartidos/AddPartidos.php
+++ AdminView/AdmPartidos/AddPartidos.php
@@ -1,66 +1,20 @@
-
 <?php
+require_once '../../Data/DataBase.php';
+require_once '../../Model/Partido/partido.php';
 
+    $db = new DB();
+    
+    // if (isset($_POST['nombre']) && isset($_POST['descripcion']) && isset($_POST['estado'])) {
+        $partido = new Partido();
+        $image = addslashes(file_get_contents($_FILES['logo']['tmp_name']));
+        $image_name = addslashes($_FILES['logo']['name']);
+        $image_size = getimagesize($_FILES['logo']['tmp_name']);
 
+        move_uploaded_file($_FILES["logo"]["tmp_name"], "upload/" . $_FILES["logo"]["name"]);
+        $partido->logo = "upload/" . $_FILES["logo"]["name"];
+        $partido->nombre = $_POST['nombre']; $partido->descripcion = $_POST['descripcion']; $partido->estado = $_POST['estado'];
+        $query = "INSERT INTO partidos(nombre,descripcion,logo,estado) VALUES('$partido->nombre','$partido->descripcion','$partido->logo','$partido->estado')";   
+        $db->connect()->query($query);
+        header('location: HomePartidos.php');
+    // }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Voto Automatizado</title>
-    <link rel="stylesheet" href="../../css/bootstrap-theme.min.css"/>
-        <link rel="stylesheet" href="../../css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="../../Design/design.css"/>
-        <link rel="stylesheet" href="../../css/Style.css"/>
-        <script src="../../js/bootstrap.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<body>
-<header>
-    <nav class="navbar navbar-dark bg-primary">
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-         <li class="nav-item active">
-          <a href="../AdminHome.php" class="btn btn-danger"><span>Volver</span></a>
-         </li>
-        </ul>
-    </nav>
-  </header>
-  <div class="main">
-            <div class="row">
-                <div class="col-text-center">
-                    <div class="col-sm-12">
-                    <form method="POST" action="../../AdminController/AdmPartidos.php" enctype="multipart/form-data">
-
-                        <div class="form-group">
-                            <input type="text" name="nombre" class="form-control" placeholder="Nombre" required/>
-                        <div>
-                        <div class="form-group">
-                            <input type="text" name="descripcion" class="form-control" placeholder="Descripcion" required/>
-                        <div>
-                                          
-                        <div class="form-group">
-                            <input type="file" name="foto" class="form-control"/>
-                        <div>
-                        <div class="form-group">
-                            <select class="form-control" name="estado" id="exampleFormControlSelect1">
-                           
-                            <option>Estado</option>
-                            <option value="1">Activo</option>
-                            <option value="0">Inactivo</option>
-                            </select>
-                           
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-dark" value="Agregar"/>
-                        <div>
-                    </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-</body>
-</html>
